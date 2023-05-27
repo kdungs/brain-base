6a30e37b7dd6110cb9e2d40a58fbb8c6d94843aa
Kevin Dungs <kevin@dun.gs>
2023-05-27 11:50:29 +0200

    Add base brain skeleton.

---
+++ README.md
@@ -0,0 +1,5 @@
+# 🧠
+
+Uses [zola](https://getzola.org/).
+
+Caveat: Every article needs to set `taxonomies.tags`. I was gonna add a check in the template but realized it's actually nice this way so I cannot forget so set tags.
---
+++ static/search.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const PREVIEW_LENGTH = 80;
+const SEARCH_OPTIONS = {
+    bool: "AND",
+    fields: {
+      title: {boost: 2},
+      body: {boost: 1},
+    },
+    expand: true,
+};
+
+function showResults(el, results) {
+    if (results.length == 0) {
+        el.innerText = "No results!";
+        return;
+    }
+    el.innerHTML = "";
+    let dl = document.createElement("dl");
+    for (const r of results) {
+        const {id, title, body} = r.doc;
+        let a = document.createElement("a");
+        a.href = id;
+        a.innerText = title;
+        let span = document.createElement("span")
+        span.classList += "score";
+        span.innerText = `(${r.score.toFixed(2)})`;
+        let dt = document.createElement("dt");
+        dt.append(a, span);
+        let dd = document.createElement("dd");
+        dd.innerText = body.slice(0, PREVIEW_LENGTH);
+        if (PREVIEW_LENGTH < body.length) {
+            dd.innerText += "...";
+        }
+        dl.append(dt, dd);
+    }
+    el.appendChild(dl);
+}
+
+(function() {
+    let elForm = document.getElementById("searchForm");
+    let elText = document.getElementById("searchText");
+    let elRes = document.getElementById("searchResults");
+    let idx = elasticlunr.Index.load(searchIndex);
+
+    let search = (term) => {
+        let res = idx.search(term, SEARCH_OPTIONS);
+        showResults(elRes, res);
+    }
+
+    elForm.addEventListener("submit", (evt) => {
+        search(elText.value);
+        evt.preventDefault();
+    });
+})();
